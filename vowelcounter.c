e5febd89cbc0492da6935632a44d1fdce0896106
kshitiz-eyyy <pokharelkshitiz5@gmail.com>
2025-03-05 14:05:19 +0000

    second commit-C skeleton file

--- vowelcounter.c
+++ vowelcounter.c
@@ -1,2 +1,31 @@
-Codee for vowelcounter
+#include <stdio.h>
+#include <ctype.h>
+
+int count_vowels(const char *str) 
+{
+    int count = 0;
+    while (*str) 
+{
+        char ch = tolower(*str);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            count++;
+        }
+        str++;
+    }
+    return count;
+}
+
+int main() 
+{
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin); 
+
+    int vowels = count_vowels(str);
+    printf("Number of vowels: %d\n", vowels);
+
+    return 0;
+}
+
 #Code for vowelcounter
